72647653ffd564a0915628631740d10113f02758
GabrielVRM <Gabriel.Reis2595@gmail.com>
2022-02-27 20:50:38 -0300

    segundo commit

--- NetflixAPI/src/App.jsx
+++ NetflixAPI/src/App.jsx
@@ -1,45 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import {makeStyles, Typography} from '@material-ui/core';
 
-function App() {
-  const [count, setCount] = useState(0)
+const useStyle = makeStyles (() => ({
 
+  root:{
+
+  },
+
+}))
+
+export default function App() {
+  const classes = useStyle();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+  <>
+   <Typography variant='body2'>
+     Hello wolrd
+   </Typography>
+  </>
   )
-}
-
-export default App
+};
